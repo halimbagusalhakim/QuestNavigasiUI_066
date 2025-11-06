93c2b625c4e961b0821f1b0540555e1794d745ca
halimbagusalhakim <halsat132@gmail.com>
2025-11-06 13:27:48 +0700

    membuat column dan memberikan beberapa modifier

--- app/src/main/java/com/example/praktikum5/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum5/view/Formulir.kt
@@ -1,12 +1,16 @@
 @file:OptIn(ExperimentalMaterial3Api::class)
 package com.example.praktikum5.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -29,5 +33,12 @@ fun FormIsian(
                     (colorResource(id = R.color.teal_700))
             )
         }
-    ){}
+    ){ isiRuang ->
+        Column(modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally)
+        {
+
+        }
+    }
 }
