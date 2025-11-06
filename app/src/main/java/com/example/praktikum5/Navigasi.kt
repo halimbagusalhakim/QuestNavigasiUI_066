d5db7f9844b07c4d0f67b00728ca34a4321bbe54
halimbagusalhakim <halsat132@gmail.com>
2025-11-06 13:25:19 +0700

    memanggil FormIsian di navigasi

--- app/src/main/java/com/example/praktikum5/Navigasi.kt
+++ app/src/main/java/com/example/praktikum5/Navigasi.kt
@@ -9,6 +9,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.praktikum5.view.FormIsian
 
 enum class Navigasi {
     Formulirku,
