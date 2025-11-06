d19c97b58b2cdff04c1b75f59a5a1fc2cb038ee5
halimbagusalhakim <halsat132@gmail.com>
2025-11-06 13:24:51 +0700

    membuat topappbar

--- app/src/main/java/com/example/praktikum5/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum5/view/Formulir.kt
@@ -3,19 +3,31 @@ package com.example.praktikum5.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.praktikum5.R
 
 @Composable
 fun FormIsian(
     JenisK:List<String> = listOf("Laki-laki", "Perempuan"),
     OnSubmitBtnClick : () -> Unit
-){
-    Scaffold (modifier= Modifier,
-        ){
-        TopAppBar(
-
-        )
-    }
+) {
+    Scaffold(
+        modifier = Modifier,
+        {
+            TopAppBar(
+                title = {
+                    Text(stringResource(id = R.string.home),
+                        color = Color.White) },
+                colors = TopAppBarDefaults.topAppBarColors
+                    (colorResource(id = R.color.teal_700))
+            )
+        }
+    ){}
 }
