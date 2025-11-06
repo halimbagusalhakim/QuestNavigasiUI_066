97c7660414f5fd4520b944c54a3b318f81923413
halimbagusalhakim <halsat132@gmail.com>
2025-11-06 14:07:53 +0700

    memamnggil fungsi tampil data

--- app/src/main/java/com/example/praktikum5/Navigasi.kt
+++ app/src/main/java/com/example/praktikum5/Navigasi.kt
@@ -10,6 +10,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.praktikum5.view.FormIsian
+import com.example.praktikum5.view.TampilData
 
 enum class Navigasi {
     Formulirku,
