d84c0585d75be4071b1bf8b7d2444fe8776a932e
halimbagusalhakim <halsat132@gmail.com>
2025-11-06 12:49:08 +0700

    membuat private function untuk navigasi cancelandbacktoformulirku

--- app/src/main/java/com/example/praktikum5/Navigasi.kt
+++ app/src/main/java/com/example/praktikum5/Navigasi.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -43,3 +44,9 @@ fun DataApp(
     }
 }
 
+private fun cancelAndBackToFormulirku(
+    navController: NavController
+){
+    navController.popBackStack(Navigasi.Formulirku.name,
+        inclusive = false)
+}
